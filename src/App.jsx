22ea8e04d11947cb094069d3658aefa24ac5e9f8
MdMehediHasan22 <mdmehedi3799@gmail.com>
2025-03-01 02:52:19 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
  
@@ -6,7 +9,11 @@ function App() {
   return (
     <>
       
-      <h1 className='text-3xl font-bold-underline bg-orange-400'>React Knowledge Cafe</h1>
+     <Header></Header>
+     <div className='md:flex'>
+     <Blogs></Blogs>
+     <Bookmarks></Bookmarks>
+     </div>
      
     </>
   )
