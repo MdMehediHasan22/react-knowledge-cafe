c271f4dec98cbf56ff55ccb7393dd1d69198feea
MdMehediHasan22 <mdmehedi3799@gmail.com>
2025-03-02 01:53:10 +0600

    created blogs and bookmarks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@
 import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div className='flex justify-between items-center p-4 mx-4 border-b'>
+        <header className='flex justify-between items-center p-4 mx-4 border-b'>
              <h1 className='text-4xl font-bold'>React Knowledge Cafe</h1>
              <img src={profile} alt="" />
-        </div>
+        </header>
     );
 };
 
